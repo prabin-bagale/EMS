b0b1b7efd77af5ee21df3164b8efd3db4328eb56
prabin bagale <prabinbagale08@gmail.com>
2025-04-29 22:05:13 +0545

    change in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Login from './components/Auth/Login'
+import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 
 const App = () => {
   return (
     <div>
-      <Login />
+      {/* <Login /> */}
+      <EmployeeDashboard />
     </div>
   )
 }
