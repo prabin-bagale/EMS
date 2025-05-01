e17bf891ae2a04a28e7502c64f6a33bb0f67293e
prabin bagale <prabinbagale08@gmail.com>
2025-05-01 21:38:30 +0545

    add Tasklist Component

---
+++ src/components/Tasklist/TaskList.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const TaskList = () => {
+  return (
+    <div id='tasklist' className=' overflow-x-auto h-[55%] flex items-center justify-start gap-5 flex-nowrap w-full py-5  mt-10'>
+      <div className='h-full w-[400px] bg-yellow-300 rounded-xl flex-shrink-0'></div>
+      <div className='h-full w-[400px] bg-yellow-300 rounded-xl flex-shrink-0'></div>
+      <div className='h-full w-[400px] bg-yellow-300 rounded-xl flex-shrink-0'></div>
+      <div className='h-full w-[400px] bg-yellow-300 rounded-xl flex-shrink-0'></div>
+      <div className='h-full w-[400px] bg-yellow-300 rounded-xl flex-shrink-0'></div>
+      
+    
+      
+    </div>
+  )
+}
+
+export default TaskList
