929d6a29312f90275e10d877e85c2f33beb2010f
prabin bagale <prabinbagale08@gmail.com>
2025-05-07 14:32:39 +0545

    Add AllTask Component

---
+++ src/components/others/AllTask.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const AllTask = () => {
+  return (
+    <div className='bg-[#1C1C1C] p-5 rounded mt-5 h-48 overflow-auto'>
+      <div className='bg-red-300 mb-2 py-2 px-4 flex justify-between rounded'>
+        <h2>Bagale</h2>
+        <h3>Make a FrontEnd design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className='bg-blue-300 mb-2 py-2 px-4 flex justify-between rounded'>
+        <h2>Bagale</h2>
+        <h3>Make a FrontEnd design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className='bg-green-300 mb-2 py-2 px-4 flex justify-between rounded'>
+        <h2>Bagale</h2>
+        <h3>Make a FrontEnd design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className='bg-yellow-300 mb-2 py-2 px-4 flex justify-between rounded'>
+        <h2>Bagale</h2>
+        <h3>Make a FrontEnd design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className='bg-purple-300 mb-2 py-2 px-4 flex justify-between rounded'>
+        <h2>Bagale</h2>
+        <h3>Make a FrontEnd design</h3>
+        <h5>Status</h5>
+      </div>
+    </div>
+  )
+}
+
+export default AllTask
