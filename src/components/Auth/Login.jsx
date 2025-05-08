6c0eb0a3774fd35e609404bd3fafc40672a77775
prabin bagale <prabinbagale08@gmail.com>
2025-05-08 15:55:04 +0545

    Change in UI

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import { useState } from 'react'
 
-const Login = () => {
-
+const Login = ({handleLogin}) => {
+  
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log('Email is:', email);
-    console.log('Password is:', password);
+    handleLogin(email, password)
     setEmail('')
     setPassword('')
     
