b29b88ed7be85c6c1a45ed9be3e435e5dc52b81a
prabin bagale <prabinbagale08@gmail.com>
2025-05-09 11:39:47 +0545

    Add Authprovider Component

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react'
+import { createContext } from 'react'
+import { getLocalStorage } from '../utils/localStorage'
+import { useState } from 'react'
+
+export const AuthContext = createContext()
+
+const AuthProvider = ({children}) => {
+  const [userData, setUserData] = useState(null)
+
+
+useEffect (() => {
+  const { employees, admin } = getLocalStorage()
+  setUserData({employees, admin})
+}, [])
+  
+  return (
+    <div>
+     <AuthContext.Provider value={userData}>
+     {children}
+     </AuthContext.Provider>
+    </div>
+  )
+}
+
+export default AuthProvider
