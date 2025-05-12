24b5583d63c320886643d6375392c566a4db8c4e
prabin bagale <prabinbagale08@gmail.com>
2025-05-12 14:06:03 +0545

    Changes in main app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,44 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
+import AdminDashboard from './components/Dashboard/AdminDashboard'
+import { useEffect } from 'react'
+import { getLocalStorage, setLocalStorage } from './utils/localStorage'
+import { useState } from 'react'
+import { AuthContext } from './context/AuthProvider'
 
 const App = () => {
+
+  const [user, setUser] = useState(null)
+  const authData = useContext(AuthContext)
+ useEffect(() => {
+   if(authData){
+    const loggedInUser = localStorage.getItem('loggedInUser')
+    if(loggedInUser){
+     setUser(loggedInUser.role)
+    }
+   } 
+  }, [authData])
+
+  const handleLogin = (email,password) => {
+    if (email =="admin@me.com" && password == "123") {
+     setUser("admin")
+     localStorage.setItem('loggedInUser', JSON.stringify({ role:'admin' }))
+    }else if(authData && authData.employees.find((e) => e.email === email && e.password === password)){
+    setUser("employee")
+    localStorage.setItem('loggedInUser', JSON.stringify({ role:'employee' }))
+    }
+    else{
+      alert("Invalid email or password")
+    }}
+
+   
+    
   return (
-    <div>
-      {/* <Login /> */}
-      <EmployeeDashboard />
-    </div>
+    <>
+      {!user ? <Login handleLogin={handleLogin}/> : ''}
+      {user == "admin" ? <AdminDashboard /> : <EmployeeDashboard />}
+    </>
   )
 }
 
