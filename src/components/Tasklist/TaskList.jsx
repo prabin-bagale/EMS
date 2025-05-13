bfeed0f053cbb7c47fb93ea07cfc2de3974034f3
prabin bagale <prabinbagale08@gmail.com>
2025-05-13 20:59:39 +0545

    add new component

--- src/components/Tasklist/TaskList.jsx
+++ src/components/Tasklist/TaskList.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const TaskList = () => {
+const TaskList = ({data}) => {
   return (
     <div id='tasklist' className=' overflow-x-auto h-[55%] flex items-center justify-start gap-5 flex-nowrap w-full py-5  mt-10'>
       <div className='h-full w-[400px] p-5 bg-red-300 rounded-xl flex-shrink-0'>
