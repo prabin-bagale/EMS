443e574c39cb2170fa71bca42bbe20cfa83d04ea
prabin bagale <prabinbagale08@gmail.com>
2025-05-15 21:12:13 +0545

    add data in this component

--- src/components/others/TaskListNumber.jsx
+++ src/components/others/TaskListNumber.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
 
-const TaskListNumber = () => {
+const TaskListNumber = ({data}) => {
   return (
-    <div className='flex mt-10 justify-between gap-5 screen'> 
+    <div className='flex mt-10 justify-between gap-5 screen'>
+      
       <div className='w-[45%] py-6 px-9 rounded-xl bg-red-400'>
-      <h2 className='text-3xl font-semibold'>0</h2>
+      <h2 className='text-3xl font-semibold'>{data.taskCount.newTask}</h2>
       <h3 className='text-3xl font-medium'>New Task</h3>
       </div>
       <div className='w-[45%] py-6 px-9 rounded-xl bg-blue-400'>
-      <h2 className='text-3xl font-semibold'>0</h2>
-      <h3 className='text-3xl font-medium'>New Task</h3>
+      <h2 className='text-3xl font-semibold'>{data.taskCount.completed}</h2>
+      <h3 className='text-3xl font-medium'>Completed Task</h3>
       </div>
       <div className='w-[45%] py-6 px-9 rounded-xl bg-green-400'>
-      <h2 className='text-3xl font-semibold'>0</h2>
-      <h3 className='text-3xl font-medium'>New Task</h3>
+      <h2 className='text-3xl font-semibold'>{data.taskCount.active}</h2>
+      <h3 className='text-3xl font-medium'>Accepted Task</h3>
       </div>
       <div className='w-[45%] py-6 px-9 rounded-xl bg-yellow-400'>
-      <h2 className='text-3xl font-semibold'>0</h2>
-      <h3 className='text-3xl font-medium'>New Task</h3>
+      <h2 className='text-3xl font-semibold'>{data.taskCount.failed}</h2>
+      <h3 className='text-3xl font-medium'>Failed Task</h3>
       </div>
     </div>
   )
