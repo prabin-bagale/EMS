3da0494dc8ed9157a22f02870ee874c83a2f2b6a
prabin bagale <prabinbagale08@gmail.com>
2025-06-22 22:55:26 +0545

    Change in Dashboard

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -7,7 +7,7 @@ const EmployeeDashboard = ({data}) => {
  
   return (
     <div className='p-10 bg-[#1C1C1C] h-screen'>
-      <h1>{data.id}</h1>
+      {/* <h1>{data.id}</h1> */}
     <Header data={data} />
     <TaskListNumber data={data}/>
     <TaskList data={data}/>
